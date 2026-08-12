df373e2649a37edaf53247aed854d540ef7d4dfc
Farseen <farsukoothradan@gmail.com>
2026-08-12 22:15:57 +0530

    authentication and firebase integrated

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import router from './routes/auth.route.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(express.json());
+app.use("/",router)
 
 app.listen(PORT, () => {
   connectDB();
