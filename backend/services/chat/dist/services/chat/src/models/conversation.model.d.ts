ec12254350dc77fe3097f1ff0dd3ce42980eb229
Farseen <farsukoothradan@gmail.com>
2026-08-18 21:37:34 +0530

    chore: stop tracking build output

--- backend/services/chat/dist/services/chat/src/models/conversation.model.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document, Model } from 'mongoose';
-export interface IConversation extends Document {
-    title: string;
-    userId: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
-declare const Conversation: Model<IConversation>;
-export default Conversation;
